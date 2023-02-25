d7e52387339ed4e649b87291bbe4056ec2785d81
Avibest <97982356+Avibest@users.noreply.github.com>
2023-02-25 16:23:24 -0600

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,27 @@
+var firebase = {
+  apiKey: "AIzaSyAErSUAlvTvOnUFXwRplgpBBT0ET6bKViw",
+  authDomain: "project-93-c7ae8.firebaseapp.com",
+  projectId: "project-93-c7ae8",
+  storageBucket: "project-93-c7ae8.appspot.com",
+  messagingSenderId: "533905753785",
+  appId: "1:533905753785:web:856592e476f5b99d5a7d68"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+  function addRoom(){
+      room_name = document.getElementById("room_name").value;
+      firebase.database().ref("/").child(room_name).update({
+            purpose : "adding room name"
+      });
+
+      localStorage.setItem("room_name", room_name);
+
+      window.location = "kwitter_page.html";
+
+}
+
+function getData(){
+      firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach} )
+}
